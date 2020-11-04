a8f6cd8a3ed372318e4fa8487e6e9bf322da90d0
binbin-tuzi <73559943+binbin-tuzi@users.noreply.github.com>
2020-11-03 16:10:27 -0800

    Add files via upload

---
+++ assets/php/index.php
@@ -0,0 +1,9 @@
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-1">
+    <div class="lead">
+        <h2><font color="blue"><i>News</i></font></h2>
+    </div>
+</div>
+  
+<p>Three papers accepted by ICDE 2021. Congrats to Saeed, Natasha and Faisal. </p>
